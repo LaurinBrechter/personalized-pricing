5263231c997e593dc46c43445cc0a305fae7611e
Laurin Brechter <brechterlaurin@gmail.com>
2025-05-18 15:03:31 +0200

    notebooks

--- results/plot_utils.py
+++ results/plot_utils.py
@@ -6,7 +6,7 @@ def plot_event_history(ax, data):
     sns.scatterplot(x="t", y="customer", data=data, hue="event", ax=ax)
     ax.grid(axis="y")
     ax.yaxis.set_major_locator(plt.MultipleLocator(1))
-    ax.set_ylim(ax.get_ylim()[0] - 0.5, ax.get_ylim()[1] + 0.5)
+    ax.set_ylim(0,60)
     ax.yaxis.set_major_locator(plt.MultipleLocator(1))
     ax.yaxis.set_minor_locator(
         plt.MultipleLocator(0.5)
